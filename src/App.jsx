04a825b4e308704591e1b2994629b2a0adf7f783
Ahmad Triadi Julianto M <ahmadtriadi265@gmail.com>
2023-12-11 19:00:48 +0800

    initial commit with some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route }
+from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
 import About from "./Pages/About";
